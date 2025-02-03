8847b07aa10103dfbbaa820097791db5d74737b6
sade216 <vlad.tok.1997@mail.ru>
2025-02-04 00:19:34 +0700

    refactor main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+
+
+import Layout from './components/Layout'
+import Header from './components/Header'
+import ErrorPage from './components/ErrorPage'
+
 import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Layout/>} />
+                <Route path="*" element={<ErrorPage/>} />
+            </Routes>
         </BrowserRouter>
     </StrictMode>,
 )
