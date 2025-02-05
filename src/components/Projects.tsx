9128c3885cb5e7ba3e8c9960f875347a63aa9c36
sade216 <vlad.tok.1997@mail.ru>
2025-02-05 14:59:56 +0700

    update projects part

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,17 @@
+import { CloudArrowUpIcon, BriefcaseIcon, CircleStackIcon, LanguageIcon, WrenchIcon, PuzzlePieceIcon, CommandLineIcon } from '@heroicons/react/20/solid'
+
+
+import sphImage from '../assets/sph-profile.png'
+
 
 function Projects() {
+
     return (
         <div id='projects' className="bg-slate-900 py-24 sm:py-32">
             <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-center text-base/7 font-semibold text-purple-400">Проекты</h2>
+                <h2 className="text-center text-base/7 font-semibold text-purple-400">
+                    Проекты
+                </h2>
                 <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
                     Моё творчество 
                 </p>
@@ -11,95 +19,119 @@ function Projects() {
                     Вот несколько проектов, над которыми я работал:
                 </p>
                 
-                <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
-                <div className="relative lg:row-span-2">
-                    <div className="absolute inset-px rounded-lg bg-slate-950 lg:rounded-l-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                    <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                        <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
-                        Mobile friendly
-                        </p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                        </p>
-                    </div>
-                    <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                        <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                        <img
-                            className="size-full object-cover object-top"
-                            src="https://tailwindui.com/plus/img/component-images/bento-03-mobile-friendly.png"
-                            alt=""
-                        />
-                        </div>
-                    </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
-                </div>
-                <div className="relative max-lg:row-start-1 ">
-                    <div className="absolute inset-px rounded-lg bg-slate-950 max-lg:rounded-t-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-                    <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                        <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">Performance</p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
-                        </p>
-                    </div>
-                    <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-                        <img
-                        className="w-full max-lg:max-w-xs"
-                        src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png"
-                        alt=""
-                        />
-                    </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
-                </div>
-                <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-                    <div className="absolute inset-px rounded-lg bg-slate-950"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
-                    <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                        <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">Security</p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
-                        Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
-                        </p>
-                    </div>
-                    <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                        <img
-                        className="h-[min(152px,40cqw)] object-cover"
-                        src="https://tailwindui.com/plus/img/component-images/bento-03-security.png"
-                        alt=""
-                        />
-                    </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
-                </div>
-                <div className="relative lg:row-span-2">
-                    <div className="absolute inset-px rounded-lg bg-slate-950 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                    <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                        <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
-                        Powerful APIs
-                        </p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-300 max-lg:text-center">
-                        Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
-                        </p>
-                    </div>
-                    <div className="relative min-h-[30rem] w-full grow">
-                        <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                        <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                            <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                            <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                                NotificationSetting.jsx
+                <div className="flex flex-col">
+                    <div className="relative sm:block lg:inline-flex gap-x-10 my-5 -ml-10 p-10">
+                            <div style={{backgroundImage: `url(${sphImage})`}} className="sm:relative lg:sticky mt-5 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
+                            <div className="lg:w-full pt-10lg:pt-0">
+                                <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-100 sm:text-5xl">
+                                    Sample Packs House
+                                </h1>
+                                <p className="mt-6 text-xl/8 text-gray-400">
+                                    Меня зовут <span className='text-purple-400'>Влад</span>, и я занимаюсь веб-разработкой уже 4 года. 
+                                    Моя страсть — это создание современных и удобных интерфейсов, которые решают реальные задачи.
+                                </p>
+                                <ul role="list" className="mt-8 space-y-8 text-gray-200">
+                                    <li className="flex gap-x-3">
+                                        <LanguageIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Язык:</strong> JavaScript
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CloudArrowUpIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Frontend/UI:</strong> React, Bootstrap, Moment, React H5 Audio Player, React Icons
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CommandLineIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Backend:</strong> Npm, Express
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <WrenchIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Api:</strong> Axios, Socket.IO, React Query
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <BriefcaseIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">State Management:</strong> Redux, Redux Thunk
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <CircleStackIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Базы данных:</strong> MongoDB, Mongoose,
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <PuzzlePieceIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                        <span>
+                                            <strong className="font-semibold text-gray-400">Дополнительно:</strong> React Router, Cloudinary, Multer, PassportJS
+                                        </span>
+                                    </li>
+                                </ul>
                             </div>
-                            <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                            </div>
-                        </div>
-                        <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
-                        </div>
                     </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                </div>
+                    {/* <div className="relative sm:block lg:inline-flex gap-x-10 my-5 -ml-10 p-10">
+                        <div className="lg:w-full pt-10lg:pt-0">
+                            <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-100 sm:text-5xl">
+                                Sample Packs House
+                            </h1>
+                            <p className="mt-6 text-xl/8 text-gray-400">
+                                Меня зовут <span className='text-purple-400'>Влад</span>, и я занимаюсь веб-разработкой уже 4 года. 
+                                Моя страсть — это создание современных и удобных интерфейсов, которые решают реальные задачи.
+                            </p>
+                            <ul role="list" className="mt-8 space-y-8 text-gray-200">
+                                <li className="flex gap-x-3">
+                                    <LanguageIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Язык:</strong> JavaScript
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CloudArrowUpIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Frontend/UI:</strong> React, Bootstrap, Moment, React H5 Audio Player, React Icons
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CommandLineIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Backend:</strong> Npm, Express
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <WrenchIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Api:</strong> Axios, Socket.IO, React Query
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <BriefcaseIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">State Management:</strong> Redux, Redux Thunk
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CircleStackIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Базы данных:</strong> MongoDB, Mongoose,
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <PuzzlePieceIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Дополнительно:</strong> React Router, Cloudinary, Multer, PassportJS
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div style={{backgroundImage: `url(${sphImage})`}} className="sm:relative lg:sticky mt-5 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
+                    </div> */}
                 </div>
             </div>
         </div>
