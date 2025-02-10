7b13f05754c2d1ac5188b117f2807e93a1a9f82f
sade216 <vlad.tok.1997@mail.ru>
2025-02-10 18:34:24 +0700

    update projects block

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,8 @@
-import { CloudArrowUpIcon, BriefcaseIcon, CircleStackIcon, LanguageIcon, WrenchIcon, PuzzlePieceIcon, CommandLineIcon } from '@heroicons/react/20/solid'
+import { CloudArrowUpIcon, BriefcaseIcon, LanguageIcon, WrenchIcon, PuzzlePieceIcon, CommandLineIcon } from '@heroicons/react/20/solid'
 
 
 import sphImage from '../assets/sph-profile.png'
+import blogImage from '../assets/p_blog.png'
 
 
 function Projects() {
@@ -21,69 +22,15 @@ function Projects() {
                 
                 <div className="flex flex-col">
                     <div className="relative sm:block lg:inline-flex gap-x-10 my-5 -ml-10 p-10">
-                            <div style={{backgroundImage: `url(${sphImage})`}} className="sm:relative lg:sticky mt-5 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
-                            <div className="lg:w-full pt-10lg:pt-0">
-                                <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-100 sm:text-5xl">
-                                    Sample Packs House
-                                </h1>
-                                <p className="mt-6 text-xl/8 text-gray-400">
-                                    Меня зовут <span className='text-purple-400'>Влад</span>, и я занимаюсь веб-разработкой уже 4 года. 
-                                    Моя страсть — это создание современных и удобных интерфейсов, которые решают реальные задачи.
-                                </p>
-                                <ul role="list" className="mt-8 space-y-8 text-gray-200">
-                                    <li className="flex gap-x-3">
-                                        <LanguageIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Язык:</strong> JavaScript
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <CloudArrowUpIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Frontend/UI:</strong> React, Bootstrap, Moment, React H5 Audio Player, React Icons
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <CommandLineIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Backend:</strong> Npm, Express
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <WrenchIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Api:</strong> Axios, Socket.IO, React Query
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <BriefcaseIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">State Management:</strong> Redux, Redux Thunk
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <CircleStackIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Базы данных:</strong> MongoDB, Mongoose,
-                                        </span>
-                                    </li>
-                                    <li className="flex gap-x-3">
-                                        <PuzzlePieceIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                        <span>
-                                            <strong className="font-semibold text-gray-400">Дополнительно:</strong> React Router, Cloudinary, Multer, PassportJS
-                                        </span>
-                                    </li>
-                                </ul>
-                            </div>
-                    </div>
-                    {/* <div className="relative sm:block lg:inline-flex gap-x-10 my-5 -ml-10 p-10">
+                        <div style={{backgroundImage: `url(${sphImage})`}} className="shadow-xl sm:relative lg:sticky mt-5 mb-2 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
                         <div className="lg:w-full pt-10lg:pt-0">
                             <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-100 sm:text-5xl">
                                 Sample Packs House
                             </h1>
                             <p className="mt-6 text-xl/8 text-gray-400">
-                                Меня зовут <span className='text-purple-400'>Влад</span>, и я занимаюсь веб-разработкой уже 4 года. 
-                                Моя страсть — это создание современных и удобных интерфейсов, которые решают реальные задачи.
+                                Дипломная работа. Целью было создать альтернативу таким сервисам как: Splice и Looperman. 
+                                Где пользователи могут загружать <span className='text-purple-400'>свою</span> не 
+                                лицензированную музыку/звуки и развивать личный профиль. Исследовать общую библиотеку и брать идеи для свои будущих проектов. 
                             </p>
                             <ul role="list" className="mt-8 space-y-8 text-gray-200">
                                 <li className="flex gap-x-3">
@@ -101,7 +48,7 @@ function Projects() {
                                 <li className="flex gap-x-3">
                                     <CommandLineIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
                                     <span>
-                                        <strong className="font-semibold text-gray-400">Backend:</strong> Npm, Express
+                                        <strong className="font-semibold text-gray-400">Backend:</strong> Npm, Express, MongoDB, PassportJS
                                     </span>
                                 </li>
                                 <li className="flex gap-x-3">
@@ -117,21 +64,55 @@ function Projects() {
                                     </span>
                                 </li>
                                 <li className="flex gap-x-3">
-                                    <CircleStackIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
-                                    <span>
-                                        <strong className="font-semibold text-gray-400">Базы данных:</strong> MongoDB, Mongoose,
-                                    </span>
-                                </li>
-                                <li className="flex gap-x-3">
                                     <PuzzlePieceIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
                                     <span>
-                                        <strong className="font-semibold text-gray-400">Дополнительно:</strong> React Router, Cloudinary, Multer, PassportJS
+                                        <strong className="font-semibold text-gray-400">Дополнительно:</strong> React Router, Cloudinary, Multer
                                     </span>
                                 </li>
                             </ul>
                         </div>
-                        <div style={{backgroundImage: `url(${sphImage})`}} className="sm:relative lg:sticky mt-5 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
-                    </div> */}
+                    </div>
+                    <div className="relative sm:block lg:inline-flex gap-x-10 my-5 -ml-10 p-10">
+                        <div className="lg:w-full pt-10lg:pt-0">
+                            <div style={{backgroundImage: `url(${blogImage})`}} className="lg:hidden sm:relative lg:sticky mt-5 mb-2 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
+                            <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-100 sm:text-5xl">
+                                Blog
+                            </h1>
+                            <p className="mt-6 text-xl/8 text-gray-400">
+                                Простое приложение для понимания работы реляционных баз данных. 
+                                И нового для меня стека <span className='text-purple-400'>Bun + Vite + Hono</span>.
+                                Суть приложения - возможность создавать, редактировать и удалять посты, а так же дополнять их комментариями.
+                            </p>
+
+                            <ul role="list" className="mt-8 space-y-8 text-gray-200">
+                                <li className="flex gap-x-3">
+                                    <LanguageIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Язык:</strong> TypeScript
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CloudArrowUpIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Frontend/UI:</strong> React, React Router, React Suite, TailwindCSS
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CommandLineIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Backend:</strong> Bun, Hono, Postgres, DrizzleORM
+                                    </span>
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <WrenchIcon aria-hidden="true" className="mt-1 size-6 flex-none text-purple-400" />
+                                    <span>
+                                        <strong className="font-semibold text-gray-400">Api/State Management:</strong> Axios, Tanstack: Store
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div style={{backgroundImage: `url(${blogImage})`}} className="shadow-xl hidden sm:hidden lg:block sm:relative lg:sticky mt-5 mb-2 h-min lg:top-40 w-full bg-center bg-cover aspect-16/9 rounded-xl "></div>
+                    </div>
                 </div>
             </div>
         </div>
