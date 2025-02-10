2b427ba51cd45582157ae473c886a5aa4436a1a1
sade216 <vlad.tok.1997@mail.ru>
2025-02-10 18:33:59 +0700

    update comments

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,4 +1,4 @@
-import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { AtSymbolIcon } from '@heroicons/react/24/outline'
 
 function Contacts() {
     return (
@@ -6,51 +6,30 @@ function Contacts() {
             <div id='contacts' className="relative isolate overflow-hidden bg-gray-950 py-16 sm:py-24 lg:py-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-                    <div className="max-w-xl lg:max-w-lg">
-                        <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
+                    <div className="max-w-xl lg:max-w-lg content-center">
+                        <h2 className="text-4xl font-semibold tracking-tight text-white">Свяжитесь со мной:</h2>
                         <p className="mt-4 text-lg text-gray-300">
-                        Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
-                        dolore.
+                            Если у вас есть идея для проекта, вы хотите обсудить сотрудничество или предложить работу, напишите мне!  
                         </p>
-                        <div className="mt-6 flex max-w-md gap-x-4">
-                        <label htmlFor="email-address" className="sr-only">
-                            Email address
-                        </label>
-                        <input
-                            id="email-address"
-                            name="email"
-                            type="email"
-                            required
-                            placeholder="Enter your email"
-                            autoComplete="email"
-                            className="min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                        />
-                        <button
-                            type="submit"
-                            className="flex-none rounded-md bg-purple-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                        >
-                            Subscribe
-                        </button>
-                        </div>
                     </div>
-                    <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-                        <div className="flex flex-col items-start">
-                        <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                            <CalendarDaysIcon aria-hidden="true" className="size-6 text-white" />
+                    <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2">
+                        <div className="flex flex-col items-start cursor-pointer hover:bg-slate-900/50 rounded-xl p-5">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                                <AtSymbolIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Gmail</dt>
+                            <dd className="mt-2 text-base/7 text-gray-400">
+                                vlad.tok.1997.ds@gmail.com
+                            </dd>
                         </div>
-                        <dt className="mt-4 text-base font-semibold text-white">Weekly articles</dt>
-                        <dd className="mt-2 text-base/7 text-gray-400">
-                            Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.
-                        </dd>
-                        </div>
-                        <div className="flex flex-col items-start">
-                        <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                            <HandRaisedIcon aria-hidden="true" className="size-6 text-white" />
-                        </div>
-                        <dt className="mt-4 text-base font-semibold text-white">No spam</dt>
-                        <dd className="mt-2 text-base/7 text-gray-400">
-                            Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.
-                        </dd>
+                        <div className="flex flex-col items-start cursor-pointer hover:bg-slate-900/50 rounded-xl p-5">
+                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                                <AtSymbolIcon aria-hidden="true" className="size-6 text-white" />
+                            </div>
+                            <dt className="mt-4 text-base font-semibold text-white">Telegram</dt>
+                            <dd className="mt-2 text-base/7 text-gray-400">
+                                @oile_xd
+                            </dd>
                         </div>
                     </dl>
                     </div>
